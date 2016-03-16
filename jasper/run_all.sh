10dace5470614a8bcbaeb2069664e9832d69560d
e-koch <koch.eric.w@gmail.com>
2016-03-15 23:14:22 -0600

    Generalize run_all.sh a bit more

--- jasper/run_all.sh
+++ jasper/run_all.sh
@@ -1,29 +1,35 @@
 
 # Submit jobs to run all comparisons
 
-# Noiseless
-qsub /home/ekoch/code_repos/TurbuStat/Examples/jasper/fiducial0_all.pbs
-qsub /home/ekoch/code_repos/TurbuStat/Examples/jasper/fiducial1_all.pbs
-qsub /home/ekoch/code_repos/TurbuStat/Examples/jasper/fiducial2_all.pbs
-qsub /home/ekoch/code_repos/TurbuStat/Examples/jasper/fiducial3_all.pbs
-qsub /home/ekoch/code_repos/TurbuStat/Examples/jasper/fiducial4_all.pbs
-qsub /home/ekoch/code_repos/TurbuStat/Examples/jasper/fiducial_comp.pbs
+NODE=1
+PROCS=12
+PMEM=4gb
+HOURS=72
 
+SCRIPT_PATH=/home/ekoch/code_repos/TurbuStat/Examples/
+
+# Noiseless
+qsub -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 $SCRIPT_PATH/jasper/fiducial0_all.pbs
+qsub -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 $SCRIPT_PATH/jasper/fiducial1_all.pbs
+qsub -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 $SCRIPT_PATH/jasper/fiducial2_all.pbs
+qsub -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 $SCRIPT_PATH/jasper/fiducial3_all.pbs
+qsub -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 $SCRIPT_PATH/jasper/fiducial4_all.pbs
+qsub -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 $SCRIPT_PATH/jasper/fiducial_comp.pbs
 
 # Noisy
-qsub /home/ekoch/code_repos/TurbuStat/Examples/jasper/fiducial0_all_noise.pbs
-qsub /home/ekoch/code_repos/TurbuStat/Examples/jasper/fiducial1_all_noise.pbs
-qsub /home/ekoch/code_repos/TurbuStat/Examples/jasper/fiducial2_all_noise.pbs
-qsub /home/ekoch/code_repos/TurbuStat/Examples/jasper/fiducial3_all_noise.pbs
-qsub /home/ekoch/code_repos/TurbuStat/Examples/jasper/fiducial4_all_noise.pbs
-qsub /home/ekoch/code_repos/TurbuStat/Examples/jasper/fiducial_comp_noise.pbs
+qsub -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 $SCRIPT_PATH/jasper/fiducial0_all_noise.pbs
+qsub -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 $SCRIPT_PATH/jasper/fiducial1_all_noise.pbs
+qsub -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 $SCRIPT_PATH/jasper/fiducial2_all_noise.pbs
+qsub -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 $SCRIPT_PATH/jasper/fiducial3_all_noise.pbs
+qsub -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 $SCRIPT_PATH/jasper/fiducial4_all_noise.pbs
+qsub -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 $SCRIPT_PATH/jasper/fiducial_comp_noise.pbs
 
 # Obs to Obs
-qsub /home/ekoch/code_repos/TurbuStat/Examples/jasper/complete_to_complete.pbs
+qsub -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 $SCRIPT_PATH/jasper/complete_to_complete.pbs
 
 # Des to Obs
-qsub /home/ekoch/code_repos/TurbuStat/Examples/jasper/des_to_complete.pbs
+qsub -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 $SCRIPT_PATH/jasper/des_to_complete.pbs
 
 # Obs to Fid
-qsub /home/ekoch/code_repos/TurbuStat/Examples/jasper/complete_to_fid.pbs
+qsub -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 $SCRIPT_PATH/jasper/complete_to_fid.pbs
 
