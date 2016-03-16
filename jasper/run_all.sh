2172ff910e71e4623b0ae59c7d08e6df1786112e
e-koch <koch.eric.w@gmail.com>
2016-03-16 09:37:58 -0600

    Set job names

--- jasper/run_all.sh
+++ jasper/run_all.sh
@@ -15,9 +15,9 @@ OUTPUT_PATH=/lustre/home/ekoch/sims/results/clean_results/
 
 for face1 in {0,2}; do
     for face2 in {0,2}; do
-        qsub -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 $SCRIPT_PATH/jasper/fiducial_submit.pbs fid_comp $face1 $face2 $DATA_PATH $ADD_NOISE $OUTPUT_PATH
+        qsub -N fiducial_comp_$face1_$face2 -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 $SCRIPT_PATH/jasper/fiducial_submit.pbs fid_comp $face1 $face2 $DATA_PATH $ADD_NOISE $OUTPUT_PATH
         for fid in {0..4}; do
-            qsub -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 $SCRIPT_PATH/jasper/fiducial_submit.pbs $fid $face1 $face2 $DATA_PATH $ADD_NOISE $OUTPUT_PATH
+            qsub -N fiducial_$fid_$face1_$face2 -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 $SCRIPT_PATH/jasper/fiducial_submit.pbs $fid $face1 $face2 $DATA_PATH $ADD_NOISE $OUTPUT_PATH
 
 # Noisy
 DATA_PATH=/lustre/home/ekoch/sims/SimSuite8_noise/
@@ -26,9 +26,9 @@ OUTPUT_PATH=/lustre/home/ekoch/sims/results/noise_same_results/
 
 for face1 in {0,2}; do
     for face2 in {0,2}; do
-        qsub -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 $SCRIPT_PATH/jasper/fiducial_submit.pbs fid_comp $face1 $face2 $DATA_PATH $ADD_NOISE $OUTPUT_PATH
+        qsub -N fiducial_noise_comp_$face1_$face2 -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 $SCRIPT_PATH/jasper/fiducial_submit.pbs fid_comp $face1 $face2 $DATA_PATH $ADD_NOISE $OUTPUT_PATH
         for fid in {0..4}; do
-            qsub -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 $SCRIPT_PATH/jasper/fiducial_submit.pbs $fid $face1 $face2 $DATA_PATH $ADD_NOISE $OUTPUT_PATH
+            qsub -N fiducial_noise_$fid_$face1_$face2 -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 $SCRIPT_PATH/jasper/fiducial_submit.pbs $fid $face1 $face2 $DATA_PATH $ADD_NOISE $OUTPUT_PATH
 
 # Obs to Obs
 qsub -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 $SCRIPT_PATH/jasper/complete_to_complete.pbs
