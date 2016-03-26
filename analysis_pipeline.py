5033d38eca3145b428eae1e3d17762a6e23ea3f0
e-koch <koch.eric.w@gmail.com>
2016-03-26 11:31:32 -0600

    Create timestamped output folders whenever the analysis is run

--- analysis_pipeline.py
+++ analysis_pipeline.py
@@ -6,10 +6,18 @@ import os
 import subprocess
 import sys
 import shutil
+from datetime import datetime
 
 '''
 Runs the basic analysis pipeline, starting from the outputted HDF5 files
+
+Creates a time-stamped folder with the expected structure.
 '''
+
+
+def timestring():
+    TimeString = datetime.now().strftime("%Y%m%d%H%M%S%f")
+    return TimeString
 
 # Specify path with results
 
@@ -22,7 +30,7 @@ if path.split("/")[-1] == "HDF5_files":
     if path == "/":
         path = "./"
 else:
-    hdf5_path = path + "/HDF5_files/"
+    hdf5_path = os.path.join(path, "HDF5_files/")
     path += "/"
 
 design_matrix = sys.argv[2]
@@ -30,9 +38,15 @@ design_matrix = sys.argv[2]
 if design_matrix == "None":
     design_matrix = None
 
-turbustat_path = sys.argv[3]
-if turbustat_path[-1] != "/":
-    turbustat_path += "/"
+scripts_path = sys.argv[3]
+
+run_name = sys.argv[4]
+
+# Redefine the path with the timestamped output folder
+path = os.path.join(path, run_name+"_"+timestring())
+os.mkdir(path)
+shutil.move(hdf5_path, path)
+hdf5_path = os.path.join(path, "HDF5_files")
 
 # Convert into combined csv files.
 
@@ -42,55 +56,55 @@ print "Converting to combined csv files."
 
 try:
     ta.convert_format(hdf5_path, 0, face2=0, design=design_matrix)
-    shutil.move(hdf5_path+"distances_0_0.csv", path)
+    shutil.move(os.path.join(hdf5_path, "distances_0_0.csv"), path)
     good_comparison.append("0_0")
 except StandardError as err:
     print err
 try:
     ta.convert_format(hdf5_path, 0, face2=1, design=design_matrix)
-    shutil.move(hdf5_path+"distances_0_1.csv", path)
+    shutil.move(os.path.join(hdf5_path, "distances_0_1.csv"), path)
     good_comparison.append("0_1")
 except StandardError as err:
     print err
 try:
     ta.convert_format(hdf5_path, 0, face2=2, design=design_matrix)
-    shutil.move(hdf5_path+"distances_0_2.csv", path)
+    shutil.move(os.path.join(hdf5_path, "distances_0_2.csv"), path)
     good_comparison.append("0_2")
 except StandardError as err:
     print err
 try:
     ta.convert_format(hdf5_path, 1, face2=0, design=design_matrix)
-    shutil.move(hdf5_path+"distances_1_0.csv", path)
+    shutil.move(os.path.join(hdf5_path, "distances_1_0.csv"), path)
     good_comparison.append("1_0")
 except StandardError as err:
     print err
 try:
     ta.convert_format(hdf5_path, 1, face2=1, design=design_matrix)
-    shutil.move(hdf5_path+"distances_1_1.csv", path)
+    shutil.move(os.path.join(hdf5_path, "distances_1_1.csv"), path)
     good_comparison.append("1_1")
 except StandardError as err:
     print err
 try:
     ta.convert_format(hdf5_path, 1, face2=2, design=design_matrix)
-    shutil.move(hdf5_path+"distances_1_2.csv", path)
+    shutil.move(os.path.join(hdf5_path, "distances_1_2.csv"), path)
     good_comparison.append("1_2")
 except StandardError as err:
     print err
 try:
     ta.convert_format(hdf5_path, 2, face2=0, design=design_matrix)
-    shutil.move(hdf5_path+"distances_2_0.csv", path)
+    shutil.move(os.path.join(hdf5_path, "distances_2_0.csv"), path)
     good_comparison.append("2_0")
 except StandardError as err:
     print err
 try:
     ta.convert_format(hdf5_path, 2, face2=1, design=design_matrix)
-    shutil.move(hdf5_path+"distances_2_1.csv", path)
+    shutil.move(os.path.join(hdf5_path, "distances_2_1.csv"), path)
     good_comparison.append("2_1")
 except StandardError as err:
     print err
 try:
     ta.convert_format(hdf5_path, 2, face2=2, design=design_matrix)
-    shutil.move(hdf5_path+"distances_2_2.csv", path)
+    shutil.move(os.path.join(hdf5_path, "distances_2_2.csv"), path)
     good_comparison.append("2_2")
 except StandardError as err:
     print err
@@ -98,7 +112,7 @@ except StandardError as err:
 # Next convert the fiducial comparisons
 
 for fil in os.listdir(hdf5_path):
-    if os.path.isfile(hdf5_path+fil) and "comparisons" in fil:
+    if os.path.isfile(hdf5_path+fil) and "fid_comp" in fil:
 
         out_name = ta.convert_fiducial(hdf5_path+fil, return_name=True)
 
@@ -127,7 +141,7 @@ os.chdir(path)
 print "Fitting model of given design."
 
 subprocess.call(['Rscript',
-                 os.path.join(turbustat_path, "Examples/FactorialAnalysis.R")])
+                 os.path.join(scripts_path, "FactorialAnalysis.R")])
 
 # This should create two output tables of the whole dataset.
 
@@ -136,18 +150,18 @@ subprocess.call(['Rscript',
 print "Running metric validation."
 
 subprocess.call(['Rscript',
-                 os.path.join(turbustat_path, "Examples/noise_validation.r"),
+                 os.path.join(scripts_path, "noise_validation.r"),
                  path, "10000"])
 
 subprocess.call(['Rscript',
-                 os.path.join(turbustat_path, "Examples/signal_validation.r"),
+                 os.path.join(scripts_path, "signal_validation.r"),
                  path, "10000"])
 
 # Finally, create the model plots
 
 print "Creating model plots."
 
-execfile(os.path.join(turbustat_path, "Examples/effect_plots.py"))
+execfile(os.path.join(scripts_path, "effect_plots.py"))
 
 # Remove PDF_AD from the list
 
