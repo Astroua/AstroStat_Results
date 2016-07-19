2b9da097bdce256c80d639ddf6c3137874510a26
e-koch <koch.eric.w@gmail.com>
2016-07-19 16:36:18 -0600

    Update to pipeline to use common free-fall time

--- analysis_pipeline.py
+++ analysis_pipeline.py
@@ -41,10 +41,30 @@ scripts_path = sys.argv[3]
 
 run_name = sys.argv[4]
 
+tstep_choice = sys.argv[5]
+
+if tstep_choice == "mean":
+    mode = 'mean'
+    des_tsteps = None
+    fid_tsteps = None
+elif tstep_choice == "freefall":
+    mode = 'choice'
+    # Timesteps for Simulation set 8 (used in the Tools 2016 paper).
+    des_tsteps = [25, 26, 21, 21, 23, 25, 21, 21, 25, 26, 21, 22, 23, 26, 21,
+                  21, 30, 30, 27, 28, 30, 30, 27, 23, 30, 30, 27, 25, 30, 30,
+                  24, 25]
+    # Make sure this matches the number of design sims
+    assert len(des_tsteps) == 32
+
+    fid_tsteps = [25, 24, 26, 26, 26]
+    assert len(fid_tsteps) == 5
+else:
+    raise ValueError("tstep_choice must be 'mean' or 'freefall'")
+
 # Redefine the path with the timestamped output folder
 path = os.path.join(path, run_name + "_" + timestring())
 os.mkdir(path)
-shutil.move(hdf5_path, path)
+shutil.copytree(hdf5_path, os.path.join(path, "HDF5_files"))
 hdf5_path = os.path.join(path, "HDF5_files")
 
 os.chdir(path)
@@ -53,58 +73,67 @@ os.chdir(path)
 
 good_comparison = []
 
-print "Converting to combined csv files."
+print("Converting to combined csv files.")
 
 try:
-    ta.convert_format(hdf5_path, 0, face2=0, design=design_matrix)
+    ta.convert_format(hdf5_path, 0, face2=0, design=design_matrix,
+                      mode=mode, tsteps=des_tsteps)
     shutil.move(os.path.join(hdf5_path, "distances_0_0.csv"), path)
     good_comparison.append("0_0")
 except StandardError as err:
     print err
 try:
-    ta.convert_format(hdf5_path, 0, face2=1, design=design_matrix)
+    ta.convert_format(hdf5_path, 0, face2=1, design=design_matrix,
+                      mode=mode, tsteps=des_tsteps)
     shutil.move(os.path.join(hdf5_path, "distances_0_1.csv"), path)
     good_comparison.append("0_1")
 except StandardError as err:
     print err
 try:
-    ta.convert_format(hdf5_path, 0, face2=2, design=design_matrix)
+    ta.convert_format(hdf5_path, 0, face2=2, design=design_matrix,
+                      mode=mode, tsteps=des_tsteps)
     shutil.move(os.path.join(hdf5_path, "distances_0_2.csv"), path)
     good_comparison.append("0_2")
 except StandardError as err:
     print err
 try:
-    ta.convert_format(hdf5_path, 1, face2=0, design=design_matrix)
+    ta.convert_format(hdf5_path, 1, face2=0, design=design_matrix,
+                      mode=mode, tsteps=des_tsteps)
     shutil.move(os.path.join(hdf5_path, "distances_1_0.csv"), path)
     good_comparison.append("1_0")
 except StandardError as err:
     print err
 try:
-    ta.convert_format(hdf5_path, 1, face2=1, design=design_matrix)
+    ta.convert_format(hdf5_path, 1, face2=1, design=design_matrix,
+                      mode=mode, tsteps=des_tsteps)
     shutil.move(os.path.join(hdf5_path, "distances_1_1.csv"), path)
     good_comparison.append("1_1")
 except StandardError as err:
     print err
 try:
-    ta.convert_format(hdf5_path, 1, face2=2, design=design_matrix)
+    ta.convert_format(hdf5_path, 1, face2=2, design=design_matrix,
+                      mode=mode, tsteps=des_tsteps)
     shutil.move(os.path.join(hdf5_path, "distances_1_2.csv"), path)
     good_comparison.append("1_2")
 except StandardError as err:
     print err
 try:
-    ta.convert_format(hdf5_path, 2, face2=0, design=design_matrix)
+    ta.convert_format(hdf5_path, 2, face2=0, design=design_matrix,
+                      mode=mode, tsteps=des_tsteps)
     shutil.move(os.path.join(hdf5_path, "distances_2_0.csv"), path)
     good_comparison.append("2_0")
 except StandardError as err:
     print err
 try:
-    ta.convert_format(hdf5_path, 2, face2=1, design=design_matrix)
+    ta.convert_format(hdf5_path, 2, face2=1, design=design_matrix,
+                      mode=mode, tsteps=des_tsteps)
     shutil.move(os.path.join(hdf5_path, "distances_2_1.csv"), path)
     good_comparison.append("2_1")
 except StandardError as err:
     print err
 try:
-    ta.convert_format(hdf5_path, 2, face2=2, design=design_matrix)
+    ta.convert_format(hdf5_path, 2, face2=2, design=design_matrix,
+                      mode=mode, tsteps=des_tsteps)
     shutil.move(os.path.join(hdf5_path, "distances_2_2.csv"), path)
     good_comparison.append("2_2")
 except StandardError as err:
@@ -117,7 +146,9 @@ for fil in os.listdir(hdf5_path):
     if os.path.isfile(full_file) and "fid_comp" in fil:
         print("Fiducial comparison: " + full_file)
         out_name = ta.convert_fiducial(full_file,
-                                       return_name=True)
+                                       return_name=True,
+                                       mode=mode,
+                                       tsteps=fid_tsteps)
 
         # shutil.move(out_name, path)
 
@@ -154,7 +185,7 @@ if "0_0" not in good_comparison and "2_2" not in good_comparison:
 
 os.chdir(path)
 
-print "Fitting model of given design."
+print("Fitting model of given design.")
 
 subprocess.call(['Rscript',
                  os.path.join(scripts_path, "FactorialAnalysis.R")])
@@ -175,7 +206,7 @@ subprocess.call(['Rscript',
 
 # Finally, create the model plots
 
-print "Creating model plots."
+print("Creating model plots.")
 
 # Remove PDF_AD from the list
 
