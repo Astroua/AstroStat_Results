77679bcdb2fad7a2dffe23a8fbd7b14c247834c9
e-koch <koch.eric.w@gmail.com>
2016-07-20 16:18:35 -0600

    Move cluster analysis functions into a common file

---
+++ jasper/analysis_funcs.py
@@ -0,0 +1,188 @@
+
+from astropy.io.fits import getdata
+import os
+import numpy as np
+import copy
+
+from turbustat.analysis import stats_wrapper
+
+
+'''
+Common functions for the different analysis scripts
+'''
+
+
+def timestep_wrapper(fiducial_timestep, testing_timestep, statistics,
+                     noise_added):
+
+    # Derive the property arrays assuming uniform noise (for sims)
+    fiducial_dataset = load_and_reduce(fiducial_timestep)
+    testing_dataset = load_and_reduce(testing_timestep)
+
+    if noise_added:
+        vca_break = -0.9
+        vcs_break = -0.5
+    else:
+        vca_break = None
+        vcs_break = -0.8
+
+    distances = stats_wrapper(fiducial_dataset, testing_dataset,
+                              statistics=statistics, multicore=True,
+                              vca_break=vca_break, vcs_break=vcs_break)
+    return distances
+
+
+def single_input(a):
+    return timestep_wrapper(*a)
+
+
+def load_and_reduce(filename, moment_folder="moments/"):
+    '''
+    Load the cube in and derive the property arrays.
+    '''
+
+    file_dict = {}
+
+    file_labels = ["_moment0", "_centroid", "_linewidth", "_intint"]
+    labels = ["moment0", "centroid", "linewidth", "integrated_intensity"]
+
+    # load the cube in
+    file_dict['cube'] = list(getdata(filename, header=True))
+
+    prefix_direc = "/".join(filename.split("/")[:-1])
+    if len(prefix_direc) != 0:
+        prefix_direc = prefix_direc + "/"
+    sim_name = filename.split("/")[-1][:-4]
+
+    for dic_lab, file_lab in zip(labels, file_labels):
+        file_dict[dic_lab] = \
+            list(getdata(os.path.join(prefix_direc, moment_folder,
+                                      sim_name + file_lab + ".fits"),
+                         0, header=True))
+
+        # And the errors
+        file_dict[dic_lab + "_error"] = \
+            list(getdata(os.path.join(prefix_direc, moment_folder,
+                                      sim_name + file_lab + ".fits"),
+                         1, header=True))
+
+    return file_dict
+
+
+def sort_distances(statistics, distances):
+    if len(statistics) > 1:
+        distance_array = np.empty((len(distances), len(statistics)))
+    elif len(statistics) == 1:
+        distance_array = np.empty((len(distances), 1))
+
+    for j, dist in enumerate(distances):
+        distance_array[j, :] = [dist[stat] for stat in statistics]
+
+    return distance_array
+
+
+def files_sorter(folder, fiducial_labels=np.arange(0, 5, 1),
+                 design_labels=np.arange(0, 32, 1), timesteps='last',
+                 faces=[0, 1, 2], suffix="fits", append_prefix=False):
+    '''
+    If the entire simulation suite is in one directory, this function
+    will spit out appropriate groupings.
+
+    Parameters
+    ----------
+    folder : str
+        Folder where data is.
+    fiducial_labels : list or numpy.ndarray, optional
+        List of the fiducial numbers.
+    design_labels : list or numpy.ndarray, optional
+        List of the design numbers.
+    timesteps : 'last' or list or numpy.ndarray, optional
+        List of timesteps to analyze. If 'last', the last timestep
+        found for each simulation is used.
+    faces : list
+        Faces of the simulations to use.
+    suffix : str, optional
+        File suffix.
+    '''
+
+    # Get the files and remove any sub-directories.
+    files = [f for f in os.listdir(folder) if not os.path.isdir(f) and
+             f[-len(suffix):] == suffix]
+
+    # Set up the dictionaries.
+    fiducials = dict.fromkeys(faces)
+    designs = dict.fromkeys(faces)
+    timestep_labels = dict.fromkeys(faces)
+    for face in faces:
+        fiducials[face] = dict((lab, []) for lab in fiducial_labels)
+        designs[face] = dict((lab, []) for lab in design_labels)
+        timestep_labels[face] = dict((lab, []) for lab in design_labels)
+
+    # Sort the files
+    for f in files:
+        if "Fiducial" in f:
+            for lab in fiducial_labels:
+                if not "Fiducial" + str(lab) + "_" in f:
+                    continue
+                for face in faces:
+                    if "_0" + str(face) + "_" in f:
+                        if append_prefix:
+                            fiducials[face][lab].append(folder + f)
+                        else:
+                            fiducials[face][lab].append(f)
+
+        elif "Design" in f:
+            for lab in design_labels:
+                if not "Design" + str(lab) + "_" in f:
+                    continue
+                for face in faces:
+                    if "_0" + str(face) + "_" in f:
+                        if append_prefix:
+                            designs[face][lab].append(folder + f)
+                        else:
+                            designs[face][lab].append(f)
+
+        else:
+            print "Could not find a category for " + f
+
+    # Sort and keep only the specified timesteps
+    _timestep_sort(fiducials, timesteps)
+    _timestep_sort(designs, timesteps, labels=timestep_labels)
+
+    return fiducials, designs, timestep_labels
+
+
+def _timestep_sort(d, timesteps, labels=None):
+    '''
+    Helper function for segmenting by timesteps.
+    '''
+    for lab in d.keys():
+        for face in d[lab].keys():
+            # Check for empty lists.
+            if d[lab][face] == []:
+                continue
+            d[lab][face].sort()
+            if timesteps == 'last':  # Grab the last one
+                if labels is not None:
+                    labels[lab][face].append(d[lab][face][-1][-16:-14])
+                d[lab][face] = d[lab][face][-1]
+            elif timesteps == 'max':  # Keep all available
+                # Reverse the order so the comparisons are between the highest
+                # time steps.
+                d[lab][face] = d[lab][face][::-1]
+            elif isinstance(timesteps, int):  # Slice out a certain section
+                d[lab][face] = d[lab][face][:timesteps]
+                if labels is None:
+                    continue
+                for val in d[lab][face]:
+                    labels[lab][face].append(val[-16:-14])
+            else:  # Make a copy and loop through the steps
+                good_files = copy.copy(d[lab][face])
+                for f in d[lab][face]:
+                    match = ["_00" + str(step) +
+                             "_" in f for step in timesteps]
+                    if not any(match):
+                        good_files.remove(f)
+                    if labels is not None:
+                        labels[lab][face].append(f[-16:-14])
+                d[lab][face] = good_files
--- jasper/output_mpi_load.py
+++ jasper/output_mpi_load.py
@@ -3,43 +3,17 @@
 
 import numpy as np
 import sys
-import os
-import copy
 from datetime import datetime
-from astropy.io.fits import getdata
 from itertools import repeat
 
-from spectral_cube import SpectralCube, LazyMask
-from astropy.wcs import WCS
 
 from turbustat.statistics import stats_wrapper, statistics_list
-from turbustat.data_reduction import Mask_and_Moments
+
+
+from analysis_funcs import (load_and_reduce, single_input, sort_distances,
+                            files_sorter)
 
 np.random.seed(248954785)
-
-
-def timestep_wrapper(fiducial_timestep, testing_timestep, statistics,
-                     noise_added):
-
-    # Derive the property arrays assuming uniform noise (for sims)
-    fiducial_dataset = load_and_reduce(fiducial_timestep)
-    testing_dataset = load_and_reduce(testing_timestep)
-
-    if noise_added:
-        vca_break = -0.9
-        vcs_break = -0.5
-    else:
-        vca_break = None
-        vcs_break = -0.8
-
-    distances = stats_wrapper(fiducial_dataset, testing_dataset,
-                              statistics=statistics, multicore=True,
-                              vca_break=vca_break, vcs_break=vcs_break)
-    return distances
-
-
-def single_input(a):
-    return timestep_wrapper(*a)
 
 
 def run_all(fiducial, simulation_runs, statistics, savename,
@@ -62,7 +36,7 @@ def run_all(fiducial, simulation_runs, statistics, savename,
 
     if verbose:
         # print "Simulation runs to be analyzed: %s" % (simulation_runs)
-        print "Started at "+str(datetime.now())
+        print "Started at " + str(datetime.now())
 
     if multi_timesteps:
         # Distances will be stored in an array of dimensions
@@ -77,7 +51,7 @@ def run_all(fiducial, simulation_runs, statistics, savename,
             timesteps = simulation_runs[key]
 
             if verbose:
-                print "On Simulation %s/%s" % (i+1, len(simulation_runs))
+                print "On Simulation %s/%s" % (i + 1, len(simulation_runs))
                 print str(datetime.now())
             if pool is not None:
 
@@ -111,7 +85,7 @@ def run_all(fiducial, simulation_runs, statistics, savename,
                                           fiducial_models=all_fiducial_models,
                                           statistics=statistics)
                     distances = [distances]
-                    distances_storage[:, i, ii:ii+1] = \
+                    distances_storage[:, i, ii:ii + 1] = \
                         sort_distances(statistics, distances).T
 
     else:
@@ -147,157 +121,10 @@ def run_all(fiducial, simulation_runs, statistics, savename,
                                       fiducial_models=all_fiducial_models,
                                       statistics=statistics)
                 distances = [distances]
-                distances_storage[:, i:i+1] = \
+                distances_storage[:, i:i + 1] = \
                     sort_distances(statistics, distances).T
 
     return distances_storage
-
-
-def sort_distances(statistics, distances):
-    if len(statistics) > 1:
-        distance_array = np.empty((len(distances), len(statistics)))
-    elif len(statistics) == 1:
-        distance_array = np.empty((len(distances), 1))
-
-    for j, dist in enumerate(distances):
-        distance_array[j, :] = [dist[stat] for stat in statistics]
-
-    return distance_array
-
-
-def files_sorter(folder, fiducial_labels=np.arange(0, 5, 1),
-                 design_labels=np.arange(0, 32, 1), timesteps='last',
-                 faces=[0, 1, 2], suffix="fits", append_prefix=False):
-    '''
-    If the entire simulation suite is in one directory, this function
-    will spit out appropriate groupings.
-
-    Parameters
-    ----------
-    folder : str
-        Folder where data is.
-    fiducial_labels : list or numpy.ndarray, optional
-        List of the fiducial numbers.
-    design_labels : list or numpy.ndarray, optional
-        List of the design numbers.
-    timesteps : 'last' or list or numpy.ndarray, optional
-        List of timesteps to analyze. If 'last', the last timestep
-        found for each simulation is used.
-    faces : list
-        Faces of the simulations to use.
-    suffix : str, optional
-        File suffix.
-    '''
-
-    # Get the files and remove any sub-directories.
-    files = [f for f in os.listdir(folder) if not os.path.isdir(f) and
-             f[-len(suffix):] == suffix]
-
-    # Set up the dictionaries.
-    fiducials = dict.fromkeys(faces)
-    designs = dict.fromkeys(faces)
-    timestep_labels = dict.fromkeys(faces)
-    for face in faces:
-        fiducials[face] = dict((lab, []) for lab in fiducial_labels)
-        designs[face] = dict((lab, []) for lab in design_labels)
-        timestep_labels[face] = dict((lab, []) for lab in design_labels)
-
-    # Sort the files
-    for f in files:
-        if "Fiducial" in f:
-            for lab in fiducial_labels:
-                if not "Fiducial"+str(lab)+"_" in f:
-                    continue
-                for face in faces:
-                    if "_0"+str(face)+"_" in f:
-                        if append_prefix:
-                            fiducials[face][lab].append(folder+f)
-                        else:
-                            fiducials[face][lab].append(f)
-
-        elif "Design" in f:
-            for lab in design_labels:
-                if not "Design"+str(lab)+"_" in f:
-                    continue
-                for face in faces:
-                    if "_0"+str(face)+"_" in f:
-                        if append_prefix:
-                            designs[face][lab].append(folder+f)
-                        else:
-                            designs[face][lab].append(f)
-
-        else:
-            print "Could not find a category for " + f
-
-    # Sort and keep only the specified timesteps
-    _timestep_sort(fiducials, timesteps)
-    _timestep_sort(designs, timesteps, labels=timestep_labels)
-
-    return fiducials, designs, timestep_labels
-
-
-def _timestep_sort(d, timesteps, labels=None):
-    '''
-    Helper function for segmenting by timesteps.
-    '''
-    for lab in d.keys():
-        for face in d[lab].keys():
-            # Check for empty lists.
-            if d[lab][face] == []:
-                continue
-            d[lab][face].sort()
-            if timesteps == 'last':  # Grab the last one
-                if labels is not None:
-                    labels[lab][face].append(d[lab][face][-1][-16:-14])
-                d[lab][face] = d[lab][face][-1]
-            elif timesteps == 'max':  # Keep all available
-                # Reverse the order so the comparisons are between the highest
-                # time steps.
-                d[lab][face] = d[lab][face][::-1]
-            elif isinstance(timesteps, int):  # Slice out a certain section
-                d[lab][face] = d[lab][face][:timesteps]
-                if labels is None:
-                    continue
-                for val in d[lab][face]:
-                    labels[lab][face].append(val[-16:-14])
-            else:  # Make a copy and loop through the steps
-                good_files = copy.copy(d[lab][face])
-                for f in d[lab][face]:
-                    match = ["_00"+str(step)+"_" in f for step in timesteps]
-                    if not any(match):
-                        good_files.remove(f)
-                    if labels is not None:
-                        labels[lab][face].append(f[-16:-14])
-                d[lab][face] = good_files
-
-
-def load_and_reduce(filename, moment_folder="moments/"):
-    '''
-    Load the cube in and derive the property arrays.
-    '''
-
-    file_dict = {}
-
-    file_labels = ["_moment0", "_centroid", "_linewidth", "_intint"]
-    labels = ["moment0", "centroid", "linewidth", "integrated_intensity"]
-
-    # load the cube in
-    file_dict['cube'] = list(getdata(filename, header=True))
-
-    prefix_direc = "/".join(filename.split("/")[:-1])
-    if len(prefix_direc) != 0:
-        prefix_direc = prefix_direc + "/"
-    sim_name = filename.split("/")[-1][:-4]
-
-    for dic_lab, file_lab in zip(labels, file_labels):
-        file_dict[dic_lab] = \
-            list(getdata(prefix_direc+moment_folder+sim_name+file_lab+".fits", 0, header=True))
-
-        # And the errors
-        file_dict[dic_lab+"_error"] = \
-            list(getdata(prefix_direc+moment_folder+sim_name+file_lab+".fits", 1, header=True))
-
-    return file_dict
 
 if __name__ == "__main__":
 
@@ -401,14 +228,14 @@ if __name__ == "__main__":
         fiducial_col = []
 
         # number of comparisons b/w all fiducials
-        num_comp = (len(fiducials[face])**2. - len(fiducials[face]))/2
+        num_comp = (len(fiducials[face])**2. - len(fiducials[face])) / 2
         # Change dim 2 to match number of time steps
         distances_storage = np.zeros((num_statistics, num_comp, 10))
         posn = 0
         prev = 0
         # no need to loop over the last one
         for fid_num, i in zip(fiducials[face].keys()[:-1],
-                              np.arange(len(fiducials[comp_face])-1, 0, -1)):
+                              np.arange(len(fiducials[comp_face]) - 1, 0, -1)):
             posn += i
             comparisons = fiducials[comp_face].copy()
 
@@ -416,14 +243,16 @@ if __name__ == "__main__":
                 del comparisons[key]
             partial_distances = \
                 run_all(fiducials[face][fid_num], comparisons,
-                        statistics, save_name, pool=pool, noise_added=noise_added,
+                        statistics, save_name, pool=pool,
+                        noise_added=noise_added,
                         multi_timesteps=multi_timesteps, verbose=True)
             distances_storage[:, prev:posn, :] = partial_distances
             prev += i
 
-            fiducial_index.extend(fiducials[comp_face].keys()[fid_num+1:])
+            fiducial_index.extend(fiducials[comp_face].keys()[fid_num + 1:])
 
-            fiducial_col.extend([posn-prev] * len(fiducials[comp_face].keys()[fid_num:]))
+            fiducial_col.extend(
+                [posn - prev] * len(fiducials[comp_face].keys()[fid_num:]))
 
         # consistent naming with non-fiducial case
         simulation_runs = fiducial_index
@@ -444,15 +273,15 @@ if __name__ == "__main__":
     # To distinguish a bit better, set the non-comparisons to zero
     distances_storage[np.where(distances_storage == 0)] = np.NaN
 
-    filename = save_name +"_fiducial"+str(fiducial_num)+"_" + str(face) + "_" + str(comp_face) + \
-        "_distance_results.h5"
+    filename = save_name + "_fiducial" + str(fiducial_num) + "_" + \
+        str(face) + "_" + str(comp_face) + "_distance_results.h5"
 
-    from pandas import DataFrame, HDFStore, concat, Series
+    from pandas import DataFrame, HDFStore, concat
 
     # Save data for each statistic in a dataframe.
     # Each dataframe is saved in a single hdf5 file
 
-    store = HDFStore(output_direc+filename)
+    store = HDFStore(output_direc + filename)
 
     for i in range(num_statistics):
         # If timesteps is 'max', there will be different number of labels
