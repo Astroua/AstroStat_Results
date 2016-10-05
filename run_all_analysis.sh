423030682c9e6558d69b4e7c36462060ef95989f
e-koch <koch.eric.w@gmail.com>
2016-10-05 15:36:07 -0600

    Add running regridded result to analysis pipeline

--- run_all_analysis.sh
+++ run_all_analysis.sh
@@ -10,6 +10,7 @@ make_folder_struct=1
 file_copy=1
 clean_analysis=1
 noisy_analysis=1
+regrid_analysis=1
 obs_analysis=1
 ff_analysis=1
 res_comp_analysis=1
@@ -21,6 +22,8 @@ if [[ ${make_folder_struct} -eq 1 ]]; then
     mkdir ${results_dir}/clean/HDF5_files
     mkdir ${results_dir}/noisy
     mkdir ${results_dir}/noisy/HDF5_files
+    mkdir ${results_dir}/regrid
+    mkdir ${results_dir}/regrid/HDF5_files
     # Observational comparison analysis
     mkdir ${results_dir}/noisy/Obs_to_Obs
     mkdir ${results_dir}/noisy/Obs_to_Fid
@@ -32,6 +35,8 @@ if [[ ${make_folder_struct} -eq 1 ]]; then
     mkdir ${results_dir}/clean_freefall/HDF5_files
     mkdir ${results_dir}/noisy_freefall
     mkdir ${results_dir}/noisy_freefall/HDF5_files
+    mkdir ${results_dir}/regrid_freefall
+    mkdir ${results_dir}/regrid_freefall/HDF5_files
     # Res comparison
     mkdir /media/eric/Data_3/Astrostat/Fiducial_256/moments
     mkdir /media/eric/Data_3/Astrostat/Fiducial_reproc/moments
@@ -43,6 +48,7 @@ if [[ ${file_copy} -eq 1 ]]; then
     # Sim comparisons
     rsync ekoch@jasper.westgrid.ca:/home/ekoch/sims/results/clean_results/*.h5 ${results_dir}/clean/HDF5_files/
     rsync ekoch@jasper.westgrid.ca:/home/ekoch/sims/results/noise_same_results/*.h5 ${results_dir}/noisy/HDF5_files/
+    rsync ekoch@jasper.westgrid.ca:/home/ekoch/sims/results/regrid_results/*.h5 ${results_dir}/regrid/HDF5_files/
 
     # Obs comparisons
     rsync ekoch@jasper.westgrid.ca:/home/ekoch/sims/results/des_to_obs/*.h5 ${results_dir}/noisy/Des_to_Obs/HDF5/
@@ -52,6 +58,7 @@ if [[ ${file_copy} -eq 1 ]]; then
     # FF comparisons
     rsync ekoch@jasper.westgrid.ca:/home/ekoch/sims/results/clean_results_freefall/*.h5 ${results_dir}/clean_freefall/HDF5_files/
     rsync ekoch@jasper.westgrid.ca:/home/ekoch/sims/results/noise_same_results_freefall/*.h5 ${results_dir}/noisy_freefall/HDF5_files/
+    rsync ekoch@jasper.westgrid.ca:/home/ekoch/sims/results/regrid_results_freefall/*.h5 ${results_dir}/regrid_freefall/HDF5_files/
 
     # Reproc fiducial comparisons
     rsync ekoch@jasper.westgrid.ca:/home/ekoch/sims/results/fiducial_reproc/*.h5 ${results_dir}/resolution_comparison
@@ -73,6 +80,13 @@ if [[ ${noisy_analysis} -eq 1 ]]; then
     python ${scripts_dir}/analysis_pipeline.py . ${results_dir}/Design7Matrix.csv ${scripts_dir} ${results_dir}/noisy/noisy mean T
 fi
 
+# Regrid
+if [[ ${regrid_analysis} -eq 1 ]]; then
+    echo "Running regrid analysis"
+    cd ${results_dir}/regrid
+    python ${scripts_dir}/analysis_pipeline.py . ${results_dir}/Design7Matrix.csv ${scripts_dir} ${results_dir}/regrid/regrid mean F
+fi
+
 # Also run the observational analysis from noisy
 # Need some way of getting the name of the newly created noise analysis folder (with timestep)
 # if [[ ${obs_analysis} -eq 0 ]]; then
@@ -84,10 +98,13 @@ fi
 if [[ ${ff_analysis} -eq 1 ]]; then
     echo "Running clean freefall analysis"
     cd ${results_dir}/clean_freefall
-    python ${scripts_dir}/analysis_pipeline.py . ${results_dir}/Design7Matrix.csv ${scripts_dir} ${results_dir}/clean_freefall/clean_freefall mean T
+    python ${scripts_dir}/analysis_pipeline.py . ${results_dir}/Design7Matrix.csv ${scripts_dir} ${results_dir}/clean_freefall/clean_freefall mean F
     echo "Running noisy freefall analysis"
     cd ${results_dir}/noisy_freefall
-    python ${scripts_dir}/analysis_pipeline.py . ${results_dir}/Design7Matrix.csv ${scripts_dir} ${results_dir}/noisy_freefall/noisy_freefall mean F
+    python ${scripts_dir}/analysis_pipeline.py . ${results_dir}/Design7Matrix.csv ${scripts_dir} ${results_dir}/noisy_freefall/noisy_freefall mean T
+    echo "Running regrid freefall analysis"
+    cd ${results_dir}/regrid_freefall
+    python ${scripts_dir}/analysis_pipeline.py . ${results_dir}/Design7Matrix.csv ${scripts_dir} ${results_dir}/regrid_freefall/regrid_freefall mean F
 fi
 
 if [[ ${res_comp_analysis} -eq 1 ]]; then
