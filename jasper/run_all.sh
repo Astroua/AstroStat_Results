d198cf8919516e6479c9bbf218761fbd7f161fc8
e-koch <koch.eric.w@gmail.com>
2017-01-09 10:17:09 -0700

    Adjust submit script to do all at once

--- jasper/run_all.sh
+++ jasper/run_all.sh
@@ -21,77 +21,80 @@ SCRIPT_PATH=/home/ekoch/code_repos/AstroStat_Results/
 # the timesteps of all others (starting from the last timestep in each).
 # If 'freefall', comparisons are only done between the timesteps in each simulation
 # nearest to a free-fall time.
-COMPARE_TYPE='max'
+# COMPARE_TYPE='max'
 # COMPARE_TYPE=freefall
 
-echo "Running comparison type: "$COMPARE_TYPE
+for COMPARE_TYPE in 'max', 'freefall'; do
 
-# Noiseless
-DATA_DIR=/lustre/home/ekoch/sims/SimSuite8/
-ADD_NOISE=F
-if [[ $COMPARE_TYPE = max ]]; then
-    RESULTS_DIR=/lustre/home/ekoch/sims/results/clean_results/
-else
-    RESULTS_DIR=/lustre/home/ekoch/sims/results/clean_results_freefall/
-fi
+    echo "Running comparison type: "$COMPARE_TYPE
 
-if [[ $run_clean -eq 1 ]]; then
-    # Run face 0 and face 2 design to fiducial comparisons
-    for face1 in {0,2}; do
-        for face2 in {0,2}; do
-            for fid in {0..4}; do
-                qsub -N fiducial_"$fid"_"$face1"_"$face2"_"$COMPARE_TYPE" -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 -v SCRIPT_PATH=$SCRIPT_PATH,FIDUCIAL=$fid,FACE_1=$face1,FACE_2=$face2,DATA_DIR=$DATA_DIR,ADD_NOISE=$ADD_NOISE,RESULTS_DIR=$RESULTS_DIR,COMPARE_TYPE=$COMPARE_TYPE $SCRIPT_PATH/jasper/fiducial_submit.pbs
+    # Noiseless
+    DATA_DIR=/lustre/home/ekoch/sims/SimSuite8/
+    ADD_NOISE=F
+    if [[ $COMPARE_TYPE = max ]]; then
+        RESULTS_DIR=/lustre/home/ekoch/sims/results/clean_results/
+    else
+        RESULTS_DIR=/lustre/home/ekoch/sims/results/clean_results_freefall/
+    fi
+
+    if [[ $run_clean -eq 1 ]]; then
+        # Run face 0 and face 2 design to fiducial comparisons
+        for face1 in {0,2}; do
+            for face2 in {0,2}; do
+                for fid in {0..4}; do
+                    qsub -N fiducial_"$fid"_"$face1"_"$face2"_"$COMPARE_TYPE" -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 -v SCRIPT_PATH=$SCRIPT_PATH,FIDUCIAL=$fid,FACE_1=$face1,FACE_2=$face2,DATA_DIR=$DATA_DIR,ADD_NOISE=$ADD_NOISE,RESULTS_DIR=$RESULTS_DIR,COMPARE_TYPE=$COMPARE_TYPE $SCRIPT_PATH/jasper/fiducial_submit.pbs
+                done
             done
         done
-    done
 
-    # Run all faces of the fiducial comparisons
-    for face1 in {0..2}; do
-        for face2 in {0..2}; do
-            qsub -N fiducial_comp_"$face1"_"$face2"_"$COMPARE_TYPE" -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 -v SCRIPT_PATH=$SCRIPT_PATH,FIDUCIAL="fid_comp",FACE_1=$face1,FACE_2=$face2,DATA_DIR=$DATA_DIR,ADD_NOISE=$ADD_NOISE,RESULTS_DIR=$RESULTS_DIR,COMPARE_TYPE=$COMPARE_TYPE $SCRIPT_PATH/jasper/fiducial_submit.pbs
-        done
-    done
-fi
-
-# Noisy
-DATA_DIR=/lustre/home/ekoch/sims/SimSuite8_noise/
-ADD_NOISE=T
-if [[ $COMPARE_TYPE = max ]]; then
-    RESULTS_DIR=/lustre/home/ekoch/sims/results/noise_same_results/
-else
-    RESULTS_DIR=/lustre/home/ekoch/sims/results/noise_same_results_freefall/
-fi
-
-if [[ $run_noise -eq 1 ]]; then
-    for face1 in {0,2}; do
-        for face2 in {0,2}; do
-            qsub -N fiducial_noise_comp_"$face1"_"$face2"_"$COMPARE_TYPE" -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 -v SCRIPT_PATH=$SCRIPT_PATH,FIDUCIAL="fid_comp",FACE_1=$face1,FACE_2=$face2,DATA_DIR=$DATA_DIR,ADD_NOISE=$ADD_NOISE,RESULTS_DIR=$RESULTS_DIR,COMPARE_TYPE=$COMPARE_TYPE $SCRIPT_PATH/jasper/fiducial_submit.pbs
-            for fid in {0..4}; do
-                qsub -N fiducial_noise_"$fid"_"$face1"_"$face2"_"$COMPARE_TYPE" -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 -v SCRIPT_PATH=$SCRIPT_PATH,FIDUCIAL=$fid,FACE_1=$face1,FACE_2=$face2,DATA_DIR=$DATA_DIR,ADD_NOISE=$ADD_NOISE,RESULTS_DIR=$RESULTS_DIR,COMPARE_TYPE=$COMPARE_TYPE $SCRIPT_PATH/jasper/fiducial_submit.pbs
+        # Run all faces of the fiducial comparisons
+        for face1 in {0..2}; do
+            for face2 in {0..2}; do
+                qsub -N fiducial_comp_"$face1"_"$face2"_"$COMPARE_TYPE" -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 -v SCRIPT_PATH=$SCRIPT_PATH,FIDUCIAL="fid_comp",FACE_1=$face1,FACE_2=$face2,DATA_DIR=$DATA_DIR,ADD_NOISE=$ADD_NOISE,RESULTS_DIR=$RESULTS_DIR,COMPARE_TYPE=$COMPARE_TYPE $SCRIPT_PATH/jasper/fiducial_submit.pbs
             done
         done
-    done
-fi
+    fi
 
-# Regridded
-DATA_DIR=/lustre/home/ekoch/sims/SimSuite8_regrid/
-ADD_NOISE=T
-if [[ $COMPARE_TYPE = max ]]; then
-    RESULTS_DIR=/lustre/home/ekoch/sims/results/regrid_results/
-else
-    RESULTS_DIR=/lustre/home/ekoch/sims/results/regrid_results_freefall/
-fi
+    # Noisy
+    DATA_DIR=/lustre/home/ekoch/sims/SimSuite8_noise/
+    ADD_NOISE=T
+    if [[ $COMPARE_TYPE = max ]]; then
+        RESULTS_DIR=/lustre/home/ekoch/sims/results/noise_same_results/
+    else
+        RESULTS_DIR=/lustre/home/ekoch/sims/results/noise_same_results_freefall/
+    fi
 
-if [[ $run_regrid -eq 1 ]]; then
-    for face1 in {0,2}; do
-        for face2 in {0,2}; do
-            qsub -N fiducial_regrid_comp_"$face1"_"$face2"_"$COMPARE_TYPE" -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 -v SCRIPT_PATH=$SCRIPT_PATH,FIDUCIAL="fid_comp",FACE_1=$face1,FACE_2=$face2,DATA_DIR=$DATA_DIR,ADD_NOISE=$ADD_NOISE,RESULTS_DIR=$RESULTS_DIR,COMPARE_TYPE=$COMPARE_TYPE $SCRIPT_PATH/jasper/fiducial_submit.pbs
-            for fid in {0..4}; do
-                qsub -N fiducial_regrid_"$fid"_"$face1"_"$face2"_"$COMPARE_TYPE" -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 -v SCRIPT_PATH=$SCRIPT_PATH,FIDUCIAL=$fid,FACE_1=$face1,FACE_2=$face2,DATA_DIR=$DATA_DIR,ADD_NOISE=$ADD_NOISE,RESULTS_DIR=$RESULTS_DIR,COMPARE_TYPE=$COMPARE_TYPE $SCRIPT_PATH/jasper/fiducial_submit.pbs
+    if [[ $run_noise -eq 1 ]]; then
+        for face1 in {0,2}; do
+            for face2 in {0,2}; do
+                qsub -N fiducial_noise_comp_"$face1"_"$face2"_"$COMPARE_TYPE" -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 -v SCRIPT_PATH=$SCRIPT_PATH,FIDUCIAL="fid_comp",FACE_1=$face1,FACE_2=$face2,DATA_DIR=$DATA_DIR,ADD_NOISE=$ADD_NOISE,RESULTS_DIR=$RESULTS_DIR,COMPARE_TYPE=$COMPARE_TYPE $SCRIPT_PATH/jasper/fiducial_submit.pbs
+                for fid in {0..4}; do
+                    qsub -N fiducial_noise_"$fid"_"$face1"_"$face2"_"$COMPARE_TYPE" -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 -v SCRIPT_PATH=$SCRIPT_PATH,FIDUCIAL=$fid,FACE_1=$face1,FACE_2=$face2,DATA_DIR=$DATA_DIR,ADD_NOISE=$ADD_NOISE,RESULTS_DIR=$RESULTS_DIR,COMPARE_TYPE=$COMPARE_TYPE $SCRIPT_PATH/jasper/fiducial_submit.pbs
+                done
             done
         done
-    done
-fi
+    fi
+
+    # Regridded
+    DATA_DIR=/lustre/home/ekoch/sims/SimSuite8_regrid/
+    ADD_NOISE=T
+    if [[ $COMPARE_TYPE = max ]]; then
+        RESULTS_DIR=/lustre/home/ekoch/sims/results/regrid_results/
+    else
+        RESULTS_DIR=/lustre/home/ekoch/sims/results/regrid_results_freefall/
+    fi
+
+    if [[ $run_regrid -eq 1 ]]; then
+        for face1 in {0,2}; do
+            for face2 in {0,2}; do
+                qsub -N fiducial_regrid_comp_"$face1"_"$face2"_"$COMPARE_TYPE" -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 -v SCRIPT_PATH=$SCRIPT_PATH,FIDUCIAL="fid_comp",FACE_1=$face1,FACE_2=$face2,DATA_DIR=$DATA_DIR,ADD_NOISE=$ADD_NOISE,RESULTS_DIR=$RESULTS_DIR,COMPARE_TYPE=$COMPARE_TYPE $SCRIPT_PATH/jasper/fiducial_submit.pbs
+                for fid in {0..4}; do
+                    qsub -N fiducial_regrid_"$fid"_"$face1"_"$face2"_"$COMPARE_TYPE" -l nodes=$NODE:ppn=$PROCS,pmem=$PMEM,walltime=$HOURS:00:00 -v SCRIPT_PATH=$SCRIPT_PATH,FIDUCIAL=$fid,FACE_1=$face1,FACE_2=$face2,DATA_DIR=$DATA_DIR,ADD_NOISE=$ADD_NOISE,RESULTS_DIR=$RESULTS_DIR,COMPARE_TYPE=$COMPARE_TYPE $SCRIPT_PATH/jasper/fiducial_submit.pbs
+                done
+            done
+        done
+    fi
+done
 
 # Only run the observational comparisons when using 'max' (to avoid re-computing)
 if [[ $run_obs -eq 1 ]]; then
