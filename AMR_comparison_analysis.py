05ff07f5a381c56c6616c3e123af1019693d6774
e-koch <koch.eric.w@gmail.com>
2017-03-15 16:05:54 -0600

    Scripts for comparing the effect of AMR and T=40 K fiducials

---
+++ AMR_comparison_analysis.py
@@ -0,0 +1,192 @@
+
+'''
+Compare the timestep 30 fiducials with and without AMR.
+'''
+
+import numpy as np
+import astropy.units as u
+import os
+import sys
+from astropy.utils.console import ProgressBar
+import pandas as pd
+from copy import copy
+import statsmodels.api as sm
+
+from turbustat.data_reduction import Mask_and_Moments
+from turbustat.statistics import statistics_list
+
+from jasper.analysis_funcs import (files_sorter, sort_distances,
+                                   timestep_wrapper)
+
+
+# sys.argv[1]  - path to the full (non-AMR) dataset
+# sys.argv[2] - path to the AMR fiducials
+# sys.argv[3] - output path to save results in
+
+path_to_data = sys.argv[1]
+moments_path = os.path.join(path_to_data, "moments/")
+
+path_to_amrdata = sys.argv[2]
+amrmoments_path = os.path.join(path_to_amrdata, "moments/")
+
+results_path = sys.argv[3]
+save_path = os.path.join(results_path, "amr_comparison")
+if not os.path.exists(save_path):
+    os.mkdir(save_path)
+
+faces = [0] # [0, 2]
+
+fiducials, _, _ = \
+    files_sorter(path_to_data, timesteps='last', faces=faces,
+                 append_prefix=True, design_labels=[], verbose=False)
+
+# Now the AMR cubes
+fiducials_amr, _, _ = \
+    files_sorter(path_to_amrdata, append_prefix=True, design_labels=[],
+                 faces=faces, timesteps='last', verbose=False)
+
+# If the AMR moments path doesn't exist, make the moment arrays and save.
+if not os.path.exists(amrmoments_path):
+
+    os.mkdir(amrmoments_path)
+
+    for face in faces:
+        for fid in fiducials_amr[face]:
+            fid_name = fiducials_amr[face][fid]
+            mask_mom = Mask_and_Moments(fid_name,
+                                        scale=0.001 * u.K)
+            mask_mom.make_moments()
+            mask_mom.make_moment_errors()
+
+            save_name = os.path.splitext(os.path.basename(fid_name))[0]
+            mask_mom.to_fits(os.path.join(amrmoments_path, save_name))
+
+
+# Now run the distances AMR vs. none.
+statistics = copy(statistics_list)
+statistics.append("DeltaVariance_Centroid_Curve")
+statistics.append("DeltaVariance_Centroid_Slope")
+
+print "Statistics to run: %s" % (statistics)
+num_statistics = len(statistics)
+
+for face in faces:
+    for fid in ProgressBar([3, 4]):
+
+        distances = np.zeros((len(statistics),
+                              len(fiducials_amr[face].keys())))
+
+        fid_name = fiducials[face][fid]
+
+        for i, amr_fid in enumerate(fiducials_amr[face].keys()):
+            fid_amr_name = fiducials_amr[face][amr_fid]
+
+            out = timestep_wrapper(fid_name, fid_amr_name, statistics, False)
+
+            out = [out]
+            distances[:, i] = sort_distances(statistics, out).T.squeeze()
+
+        df = pd.DataFrame(distances, index=statistics).T
+
+        # Save each fiducial as a csv file
+        save_name = "SimSuite8_fiducial{0}_amr_comparison_" \
+            "face_{1}.csv".format(fid, face)
+        df.to_csv(os.path.join(save_path, save_name))
+
+    # And the distances between the AMR fiducials.
+
+    dists = {}
+
+    for fid in ProgressBar(fiducials_amr[face].keys()):
+
+        fid_name = fiducials_amr[face][fid]
+
+        for i, amr_fid in enumerate(fiducials_amr[face].keys()):
+            if amr_fid == fid:
+                continue
+
+            fid_amr_name = fiducials_amr[face][amr_fid]
+
+            out = timestep_wrapper(fid_name, fid_amr_name, statistics, False)
+
+            out = [out]
+            dists["{0}_{1}".format(fid, amr_fid)] = \
+                sort_distances(statistics, out).T.squeeze()
+
+    df_fids = pd.DataFrame(dists, index=statistics)
+
+    # Save each fiducial as a csv file
+    save_name = "SimSuite8_fiducial_to_fiducial_amr_comparison_" \
+        "face_{0}.csv".format(face)
+    df_fids.to_csv(os.path.join(save_path, save_name))
+
+# Now run the analysis.
+
+# Need the path to the most recent clean run
+clean_path = os.path.join(results_path, "clean")
+folds = [os.path.join(clean_path, fold) for fold in
+         os.listdir(clean_path) if "clean" in fold]
+folds.sort()
+print("Using the clean results from: {}".format(folds[-1]))
+path_to_128dists = folds[-1]
+
+niters = 10000
+
+for face in faces:
+    print("Running on face {}.".format(face))
+
+    # non-AMR to non-AMR
+    fid_fid = pd.read_csv(os.path.join(folds[-1],
+                                       "fiducials_{0}_{0}.csv".format(face)))
+
+    # AMR to AMR
+    filename = "SimSuite8_fiducial_to_fiducial_amr_comparison_" \
+        "face_{}.csv".format(face)
+    amr_amr = pd.read_csv(os.path.join(save_path, filename))
+
+    # Load in each fiducial comparison and stack
+    for fid in np.arange(0, 5):
+
+        name = "SimSuite8_fiducial{0}_amr_comparison_face_" \
+            "{1}.csv".format(fid, face)
+        df = pd.read_csv(os.path.join(save_path, name))
+        # Add in row to identify which fiducial this is
+        df["Fiducial"] = pd.Series(np.array([fid] * 5))
+
+        if fid == 0:
+            amr_fid = df
+        else:
+            amr_fid = pd.concat([amr_fid, df])
+
+    pvals = dict.fromkeys(amr_amr.columns)
+
+    for stat in ProgressBar(pvals.keys()):
+
+        # Stack all distances together w/ a dummy variable
+        # QUESTION: Treat the testing against the same as one group?
+        dists = np.hstack([amr_fid[stat], amr_amr[stat], fid_fid[stat]])
+        x = np.hstack([np.ones_like(amr_fid[stat]),
+                       np.zeros_like(amr_amr[stat]),
+                       np.ones_like(fid_fid[stat])])
+                       # 2 * np.ones_like(fid_fid[stat])])
+
+        x = sm.add_constant(x)
+
+        model = sm.OLS(dists, x)
+        results = model.fit()
+        mean_diff = results.params[1]
+
+        # Now permute and re-test
+        counter = 0
+        for _ in xrange(niters):
+            perm_dists = np.random.permutation(dists)
+            perm_results = sm.OLS(perm_dists, x).fit()
+            if perm_results.params[1] > mean_diff:
+                counter += 1
+        pvals[stat] = float(counter) / float(niters)
+
+    pval_df = pd.DataFrame(pvals, index=[0])
+
+    output_name = os.path.join(save_path,
+                               "pvals_face_{}.csv".format(face))
+    pval_df.to_csv(output_name)
---
+++ hot_compare_analysis.py
@@ -0,0 +1,167 @@
+
+import numpy as np
+import analysis as ta
+from turbustat.statistics import statistics_list
+import os
+import subprocess
+import sys
+import shutil
+from datetime import datetime
+
+'''
+Runs the analysis for comparisons between the hot fiducials,
+starting from the outputted HDF5 files
+
+Creates a time-stamped folder with the expected structure.
+'''
+
+
+def timestring():
+    TimeString = datetime.now().strftime("%Y%m%d%H%M%S%f")
+    return TimeString
+
+
+# Specify path with results
+path = os.path.abspath(sys.argv[1])
+
+if path.split("/")[-1] == "HDF5_files":
+    hdf5_path = path + "/"
+    path = "/".join(path.split("/")[:-1]) + "/"
+    if path == "/":
+        path = "./"
+else:
+    hdf5_path = os.path.join(path, "HDF5_files/")
+    path += "/"
+
+scripts_path = sys.argv[2]
+
+run_name = sys.argv[3]
+
+tstep_choice = sys.argv[4]
+
+added_noise = True if sys.argv[5] == "T" else False
+
+faces = sys.argv[6].split()
+
+faces = ["{0}_{0}".format(face) for face in faces]
+
+statistics = statistics_list
+statistics.append("DeltaVariance_Centroid_Slope")
+statistics.append("DeltaVariance_Centroid_Curve")
+
+
+if tstep_choice == "mean":
+    mode = 'mean'
+    des_tsteps = None
+    fid_tsteps = None
+# elif tstep_choice == "freefall":
+#     mode = 'choice'
+#     # Timesteps for Simulation set 8 (used in the Tools 2016 paper).
+#     des_tsteps = \
+#         np.array([25, 26, 21, 21, 23, 25, 21, 21, 25, 26, 21, 22, 23, 26, 21,
+#                   21, 30, 30, 27, 28, 30, 30, 27, 23, 30, 30, 27, 25, 30, 30,
+#                   24, 25])
+#     # Make sure this matches the number of design sims
+#     assert len(des_tsteps) == 32
+
+#     fid_tsteps = np.array([25, 24, 26, 26, 26])
+#     assert len(fid_tsteps) == 5
+
+#     # Subtract the 'zeroth' timestep to get the right index
+#     des_tsteps -= 21
+#     fid_tsteps -= 21
+
+else:
+    # raise ValueError("tstep_choice must be 'mean' or 'freefall'")
+    raise ValueError("tstep_choice must be 'mean'.")
+
+# Redefine the path with the timestamped output folder
+path = os.path.join(path, run_name + "_" + timestring())
+os.mkdir(path)
+shutil.copytree(hdf5_path, os.path.join(path, "HDF5_files"))
+hdf5_path = os.path.join(path, "HDF5_files")
+
+os.chdir(path)
+
+# Convert into combined csv files.
+
+good_comparison = []
+
+print("Converting to combined csv files.")
+
+for face1 in [0, 1, 2]:
+    for face2 in [0, 1, 2]:
+        try:
+            ta.convert_format(hdf5_path, face1, face2=face2,
+                              design=None,
+                              mode=mode, tsteps=des_tsteps)
+            shutil.move(os.path.join(hdf5_path,
+                                     "distances_{0}_{1}.csv".format(face1,
+                                                                    face2)),
+                        path)
+            good_comparison.append("{0}_{1}".format(face1, face2))
+        except StandardError as err:
+            print err
+
+# Next convert the fiducial comparisons
+
+for fil in os.listdir(hdf5_path):
+    full_file = os.path.join(hdf5_path, fil)
+    if os.path.isfile(full_file) and "fid_comp" in fil:
+        print("Fiducial comparison: " + full_file)
+        out_name = ta.convert_fiducial(full_file,
+                                       return_name=True,
+                                       mode=mode,
+                                       tsteps=fid_tsteps)
+
+        # shutil.move(out_name, path)
+
+# Now make the distance plots.
+
+print("Making distance plots.")
+
+# Check that the requested face files were found
+for face in faces:
+    if face in good_comparison:
+        continue
+    raise ValueError("Face {0} not found. Those found are:"
+                     " {1}".format(face, good_comparison))
+
+if not os.path.exists(os.path.join(path, "Distance_Plots")):
+    os.mkdir(os.path.join(path, "Distance_Plots"))
+
+ta.comparison_plot(path, comparisons=faces,
+                   out_path=os.path.join(path, "Distance_Plots"),
+                   design_matrix=None,
+                   statistics=statistics)
+
+if not os.path.exists(os.path.join(path, "Distance_Plots_Paper")):
+    os.mkdir(os.path.join(path, "Distance_Plots_Paper"))
+
+
+ta.comparison_plot(path, comparisons=faces,
+                   out_path=os.path.join(path, "Distance_Plots_Paper"),
+                   design_matrix=None,
+                   num_fids=5,
+                   statistics=statistics)
+
+os.chdir(path)
+
+# This isn't robust. Assumes that when only 1 face is used, it is the 0 0
+# comparison
+if len(faces) == 1:
+    print("Only found one face: {}".format(faces))
+    noise_script = "noise_validation_oneface.r"
+else:
+    print("Found multiple faces: {}".format(faces))
+    noise_script = "noise_validation.r"
+
+# Now run the metric validation
+
+print "Running metric validation."
+
+subprocess.call(['Rscript',
+                 os.path.join(scripts_path, noise_script),
+                 "10000"])
+
+print("Finished!")
